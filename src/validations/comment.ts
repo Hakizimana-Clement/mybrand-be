829c823bc3102cfd87861f23bbe92d33a5cbb2a1
Hakizimana Clement <h.clementoert@gmail.com>
2024-03-26 12:50:22 +0200

    update test v1

--- src/validations/comment.ts
+++ src/validations/comment.ts
@@ -3,7 +3,7 @@ import Joi from "joi";
 
 // step 2. import model schema to work on
 const commentSchema = Joi.object({
-  name: Joi.string().required().min(4).max(10),
+  name: Joi.string().required().min(4).max(20),
   email: Joi.string().required().email(),
   comment: Joi.string().required().max(500),
 });
